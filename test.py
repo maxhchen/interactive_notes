8a1b5fc0d099c6c571e4d2c0e90d7d7bdb9f66d8
Maxwell Chen <maxhchen@gmail.com>
2021-05-25 01:13:01 -0700

    fix imaginary number plotting

--- test.py
+++ test.py
@@ -41,53 +41,52 @@ axcolor = 'lightgoldenrodyellow'
 plt.subplots_adjust(left=0.25, bottom=0.25)
 
 # Make a vertically oriented slider to control the imaginary value
-ax_im = plt.axes([0.1, 0.25, 0.0225, 0.63], facecolor=axcolor)
-imaginary_slider = Slider(
-    ax=ax_im,
+# ax_im = plt.axes([0.1, 0.25, 0.0225, 0.63], facecolor=axcolor)
+imaginary_slider = st.sidebar.slider(
+    # ax=ax_im,
     label="Imaginary axis",
-    valmin=-10,
-    valmax=10,
-    valinit=init_im,
-    orientation="vertical"
+    min_value=-10,
+    max_value=10,
+    # valinit=init_im,
+    # orientation="vertical"
 )
 
 # Make a horizontal slider to control the real value.
-ax_real = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-real_slider = Slider(  
-    ax=ax_real,
+# ax_real = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+real_slider = st.sidebar.slider(  
+    # ax=ax_real,
     label='Real Axis',
-    valmin=-10,
-    valmax=10,
-    valinit=init_real,
+    min_value=-10,
+    max_value=10,
+    # valinit=init_real,
 )
 
-
 # The function to be called anytime a slider's value changes
-def update(val):
-    axis.clear()
-    plt.axis("on")
-    plt.grid()
-    plt.text(-0.15*w, 0.8*w, "Im", fontsize=14)
-    plt.text(0.8*w,-0.15*w, "Re", fontsize=14)
+# def update(val):
+axis.clear()
+plt.axis("on")
+plt.grid()
+plt.text(-0.15*w, 0.8*w, "Im", fontsize=14)
+plt.text(0.8*w,-0.15*w, "Re", fontsize=14)
 
 
-    # The real and imaginary axis
-    plt.xlim(-w,w)
-    plt.ylim(-w,w)
-    plt.arrow(0, -w, 0, 1.9*w, head_width=w/20, head_length=w/20, fc='k', ec='k')
-    plt.arrow(-w, 0, 1.9*w, 0, head_width=w/20, head_length=w/20, fc='k', ec='k')
+# The real and imaginary axis
+plt.xlim(-w,w)
+plt.ylim(-w,w)
+plt.arrow(0, -w, 0, 1.9*w, head_width=w/20, head_length=w/20, fc='k', ec='k')
+plt.arrow(-w, 0, 1.9*w, 0, head_width=w/20, head_length=w/20, fc='k', ec='k')
 
-    # plot the vector
-    z = real_slider.val + imaginary_slider.val * 1j 
-    print(z)
-    fi_a = np.angle(z)
-    x = z.real -abs(w)/20*np.cos(fi_a)
-    y = z.imag-abs(w)/20*np.sin(fi_a)
-    plt.arrow(0, 0, x, y, head_width=w/30, head_length=w/30, fc='b', ec='b')
-    fig.canvas.draw_idle()
-
+# plot the vector
+z = real_slider + imaginary_slider * 1j 
+print(z)
+fi_a = np.angle(z)
+x = z.real -abs(w)/20*np.cos(fi_a)
+y = z.imag-abs(w)/20*np.sin(fi_a)
+plt.arrow(0, 0, x, y, head_width=w/30, head_length=w/30, fc='b', ec='b')
+fig.canvas.draw_idle()
 
 # register the update function with each slider
-real_slider.on_changed(update)
-imaginary_slider.on_changed(update)
-plt.show()
+# real_slider.on_changed(update)
+# imaginary_slider.on_changed(update)
+# plt.show()
+st.pyplot(plt)
