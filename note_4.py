f7dd1b7b61545636b07b473dd8cfd2b93ddc28b0
Gavin Liu <nareauphol.liu@berkeley.edu>
2021-05-26 00:55:28 -0700

    note 4 sup + utils done

--- note_4.py
+++ note_4.py
@@ -1,50 +1,120 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import pandas as pd
-import numpy as np
-import altair as alt
-from math import log10
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
-from note_j_utils import * 
+from note_4_utils import * 
 
 ## CHANGELOG ##
 # 2021/05: Streamlit
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-st.title("Note 4 Supplement: Visualizing Complex Numbers")
+amp_slider = st.sidebar.slider(  
+    label='Signal Amplitude',
+    min_value=0.0,
+    max_value=10.0,
+    value = 5.0
+)
 
-# Cartersian Form Visualization
-st.header("1. Complex Number Visualization (Cartesian Form)")
+phase_slider = st.sidebar.slider(
+    label="Signal Phase (radians)",
+    min_value = 0.0,
+    max_value= 2*np.pi,
+    value = 5.0
+)
+
+w_slider = st.sidebar.slider(
+    label="Signal Frequency (w)",
+    min_value = 0.0,
+    max_value= 10.0,
+    value = 5.0
+)
+
+r_lp_slider = st.sidebar.slider(  
+        label='R_LP (ohms)',
+        min_value=0.0,
+        max_value=4.0,
+        value = 1.0
+    )
+
+c_lp_slider = st.sidebar.slider(
+    label="C_LP (farads)",
+    min_value = 0.0,
+    max_value= 4.0,
+    value = 1.0
+)
+
+r_hp_slider = st.sidebar.slider(  
+        label='R_HP (ohms)',
+        min_value=0.0,
+        max_value=4.0,
+        value = 1.0
+    )
+
+c_hp_slider = st.sidebar.slider(
+    label="C_HP (farads)",
+    min_value = 0.0,
+    max_value= 4.0,
+    value = 1.0
+)
+
+st.title("Note 4 Supplement: Visualizing Filters")
+
+# LP Filter Visualization
+st.header("1. Low Pass Filters")
 st.write("""
- As shown in the graphic below, a complex number $z = a + bj$ has a value of $a$ along the 
-real axis and a value of $b$ along the imaginary axis. 
-""")
-
-st.write("""
-Play around with the graphic below to get a better feel for complex numbers in Cartesian Form!
-You can adjust the complex number plotted here with the real and
-imaginary axis on the sidebar. 
-""")
-
-complex_vector_fig, real_slider, imag_slider = plot_complex_vector()
-st.pyplot(complex_vector_fig)
-
-# Polar form Visualization
-st.header("2. Polar Form Visualization ")
-st.write(""" A complex number $z = a + bj$ can also be written equivalently as:
+    This visualization is intended to help understand low-pass filters better for any given sinusoidal input. 
+    Recall that any sinusoidal input signal can be represented with the form: 
 """)
 st.latex(r'''
-    z = |z|e^{j\angle{z}}
+    V(t) = V_o cos(wt + \theta)
 ''')
-st.write(""" in polar form using the magnitude and the angle of $z$ to represent the complex number.
-""")
 st.write("""
-You can adjust the same sidebar on the left to see how changing the real and imaginary axis of the complex number
-$z$ affects the phase and magnitude of its polar representation
+    Play around with the graphic below to get a better feel of how the input signal is affected by the filter 
+    by adjusting $w$, $R$, and $C$ values of the basic RC low pass filter. 
+    You can adjust low-pass frequency, $R$, and $C$ values on the sidebar. 
+    The input signal can also be adjusted on the left by changing its amplitude, phase, and frequency
 """)
-polar_vector_fig = plot_polar_vector()
-st.pyplot(polar_vector_fig)
+
+fig1, axis1 = plt.subplots()
+t = np.linspace(0, 1, 1000)
+axis1.plot(t, signal(t, amp_slider, phase_slider, w_slider), lw=2)
+axis1.set_xlabel("Time (seconds)")
+axis1.set_ylabel("Voltage (V) ")
+
+lp_filter_fig = plot_lp_filter(t, axis1, amp_slider, phase_slider, w_slider, r_lp_slider, c_lp_slider)
+st.pyplot(lp_filter_fig)
+
+
+
+# HP Filter Visualization
+st.header("2. High Pass Filters")
+st.write("""
+    Similar to the RC low-pass filter above, play around with the graphic below to get a better feel of how the input signal is 
+    affected by the high-pass RC filter now.
+""")
+
+fig2, axis2 = plt.subplots()
+t = np.linspace(0, 1, 1000)
+axis2.plot(t, signal(t, amp_slider, phase_slider, w_slider), lw=2)
+axis2.set_xlabel("Time (seconds)")
+axis2.set_ylabel("Voltage (V) ")
+hp_filter_fig = plot_hp_filter(t, axis2, amp_slider, phase_slider, w_slider, r_hp_slider, c_hp_slider)
+st.pyplot(hp_filter_fig)
+
+st.header("3. Band Pass Filters")
+st.write("""
+    Similar to the low-pass and high-pass filters above, play around with the graphic below to get a better feel of how the input signal is 
+    affected by the bandpass filter now. Recall that a bandpass filter is just a low-pass filter cascaded with a high-pass filter.
+""")
+fig3, axis3 = plt.subplots()
+t = np.linspace(0, 1, 1000)
+axis3.plot(t, signal(t, amp_slider, phase_slider, w_slider), lw=2)
+axis3.set_xlabel("Time (seconds)")
+axis3.set_ylabel("Voltage (V) ")
+bp_filter_fig = plot_bp_filter(t, axis3, amp_slider, phase_slider, w_slider, r_hp_slider, c_hp_slider)
+st.pyplot(bp_filter_fig)
+
 
 for i in range(5):
     st.text("")
@@ -53,3 +123,4 @@ for i in range(5):
 st.write("""
 Created by Maxwell Chen and Gavin Liu
 """)
+
--- note_4_utils.py
+++ note_4_utils.py
@@ -12,3 +12,46 @@ from note_j_utils import *
 
 ## CHANGELOG ##
 # 2021/05: Streamlit
+
+
+
+
+def plot_lp_filter(t, axis, orig_sig_amp, orig_sig_phase, w, R, C):
+    transfer_function = 1/(1+ (w*R*C*1j))
+    tf_mag = np.abs(transfer_function)
+    tf_phase = np.angle(transfer_function)
+
+    new_amp = orig_sig_amp * tf_mag
+    new_phase = orig_sig_phase + tf_phase
+    axis.plot(t, signal(t, new_amp, new_phase, w), lw=3)
+    axis.legend(['Original Signal', 'Filtered Signal'])
+    
+
+
+
+def plot_hp_filter(t, axis, orig_sig_amp, orig_sig_phase, w, R, C):
+    transfer_function = (w*R*C*1j)/(1+ (w*R*C*1j))
+    tf_mag = np.abs(transfer_function)
+    tf_phase = np.angle(transfer_function)
+
+    new_amp = orig_sig_amp * tf_mag
+    new_phase = orig_sig_phase + tf_phase
+    axis.plot(t, signal(t, new_amp, new_phase, w), lw=3)
+    axis.legend(['Original Signal', 'Filtered Signal'])
+
+def plot_bp_filter(t, axis, orig_sig_amp, orig_sig_phase, w, R, C):
+    transfer_function = (1/(1+ (w*R*C*1j))) * ((w*R*C*1j)/(1+ (w*R*C*1j))) # LP * HP transfer function
+    tf_mag = np.abs(transfer_function)
+    tf_phase = np.angle(transfer_function)
+
+    new_amp = orig_sig_amp * tf_mag
+    new_phase = orig_sig_phase + tf_phase
+    axis.plot(t, signal(t, new_amp, new_phase, w), lw=3)
+    axis.legend(['Original Signal', 'Filtered Signal'])
+
+
+
+
+# The parametrized function to be plotted
+def signal(t, amplitude, phase, frequency):
+    return amplitude * np.sin(2 * np.pi * frequency * t + phase)
